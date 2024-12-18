1f78291de47bfc2c7ad41a9b18409e46af9efbd3
annaeckman <annaeckman@icloud.com>
2024-12-18 12:41:26 -0800

    import and set up fonts

--- src/utils/copy.tsx
+++ src/utils/copy.tsx
@@ -13,7 +13,7 @@ export const title: Copy = {
 export const claudiaDescription: Copy = {
   en: (
     <>
-      Claudia Cuentas is a Peruvian immigrant, a bilingual and bicultural artist, a license marriage and family therapist, a researcher and an educator, specializing in the intersectionality of art, healing trauma, trauma recovery, cultural identity, indigenous knowledge and decolonization of healing. <Link to="services">services</Link>
+      My name is Claudia Cuentas. I am a Peruvian immigrant, a bilingual and bicultural artist, a license marriage and family therapist, a researcher and an educator. I specialize in the intersectionality of art, healing trauma, trauma recovery, cultural identity, indigenous knowledge and decolonization of healing. <Link to="services">services</Link>
     </>
   ),
   es: (<>Claudia Cuentas es una inmigrante peruana, artista bilingüe y bicultural, terapeuta matrimonial y familiar con licencia, investigadora y educadora, especializada en la interseccionalidad del arte, la curación del trauma, la recuperación del trauma, la identidad cultural, el conocimiento indígena y la descolonización de la curación. <Link to="services">services</Link></>),
